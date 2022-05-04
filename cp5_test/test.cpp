424b6ce7162b44118ff31cb94bd0f85b05f87019
Ryuma Nakahata <rnakaha2@binghamton.edu>
2022-05-04 00:08:04 -0400

    testing

--- cp5_test/test.cpp
+++ cp5_test/test.cpp
@@ -4,124 +4,178 @@
 #include <unistd.h>
 #include <vector>
 using namespace std;
+//Info stores the name and Index
+//Node stores Info and whether the Node is a Player, Team, Year
+  // Additionally, Node stores two vectors, Adj Players and Adj Team/Year
+
+
+class Info{
+  public:
+    Info(){
+      name = "";
+      index = -1;
+    }
+    Info(string str, int idx){
+      name = str;
+      index = idx;
+    }
+    string getName(){return name;}
+    int getIndex(){return index;}
+  private:
+    string name = "";
+    int index = -1;
+};
 
 class Node{
   public:
     Node(){
-      name = "";
+      type = -1;
     }
-    Node(string str){
-      name = str;
+    Node(Info info, int key){ // key = 0,1,2 -> 0 = player, 1 = team, 2 = year
+      data = info;
+      type = key;
     }
-    string getName(){return name;}
-    vector<string>& getTeamVec(){return team;}
-    vector<string>& getPlayerVec(){return player;}
+    string getName(){return data.getName();}
+    int getIndex(){return data.getIndex();}
+    vector<Info>& getTeamVec(){return team;}
+    vector<Info>& getPlayerVec(){return player;}
 
   private:
-    string name = "";
-    vector<string> team;
-    vector<string> player;
+    int type = -1;
+    Info data;
+    vector<Info> team;
+    vector<Info> player;
 };
 
-void addTeamEdge(Node adj[], int index1, string obj1, int index2, string obj2)
-{
-    adj[index1].getTeamVec().push_back(obj2);
-    //adj[index2].getTeamVec().push_back(obj1);
+void addTeamEdge(vector<Node>& adj, Info obj1, Info obj2){ //when adding edges, we got to index of obj1, and append obj2 to the Team Vector of Info
+  adj.at(obj1.getIndex()).getTeamVec().push_back(obj2);
 }
 
-void addPlayerEdge(Node adj[], int index1, string obj1, int index2, string obj2)
-{
-    adj[index1].getPlayerVec().push_back(obj2);
-    adj[index2].getPlayerVec().push_back(obj1);
+void addPlayerEdge(vector<Node>& adj, Info obj1, Info obj2){//when adding edges, we got to index of obj1, and append obj2 to the Player Vector of Info
+  adj.at(obj1.getIndex()).getPlayerVec().push_back(obj2);
+  adj.at(obj2.getIndex()).getPlayerVec().push_back(obj1);
 }
 
-void printGraph(Node adj[], int V)
-{
-    for (int v = 0; v < V; ++v) {
-        cout << endl << " Adjacency list of vertex " << v << endl;
-        cout << "Team/Year  " << adj[v].getName();
-        for (string x : adj[v].getTeamVec())
-            cout << "-> " << x;
-        cout << endl << "Player     " << adj[v].getName();;
-        for (string x : adj[v].getPlayerVec())
-            cout << "-> " << x;
-        printf("\n");
-    }
+void printGraph(vector<Node>& adj){ //simply look through the two Vectors inside Node
+  for (int v = 0; v < adj.size(); ++v) {
+    cout << endl << "Adjacency list of vertex " << v << endl;
+    //print list of adj Team/Year
+    cout << " Team/Year  " << adj.at(v).getName();
+    for (Info x : adj.at(v).getTeamVec())
+      cout << "-> " << x.getName();
+    //print list of adj Player
+    cout << endl << " Player     " << adj.at(v).getName();;
+    for (Info x : adj.at(v).getPlayerVec())
+      cout << "-> " << x.getName();
+    cout << endl;
+  }
 }
  
 int main (int argc, char *argv[]){
-  //make these hold indexes/ids
-  string LA = "LA"; //0 --> struct that stores index and name
-  string BC = "BC"; //1
-  string MB = "MB"; //2
-  string BC2022 = "2022"; //3
-  string BC2021 = "2021"; //4
-  string BC2020 = "2020"; //5
-  string LA2019 = "2019"; //6
-  string LA2018 = "2018"; //7
-  string LA2017 = "2017"; //8
-  string MB2016 = "2016"; //9
-  string Mike = "Mike"; //10
-  string Tim = "Tim"; //11
-  string Katie = "Katie"; //12
-  string Callisto = "Callisto"; //13
-  string Yu = "Yu"; //14
-  string Necati = "Necati"; //15
+  //Initalize adjacency list of Nodes
+  //-> Adjaceny List will be a vector of Nodes
+  //--> Nodes will have two vectors (Player adj Vector and Team adj Vector) + any Info needed
+  vector<Node> adj;
 
-  int V = 16;
-  Node adj[V];
-  adj[0] = Node(LA);
-  adj[1] = Node(BC);
-  adj[2] = Node(MB);
-  adj[3] = Node(BC2022);
-  adj[4] = Node(BC2021);
-  adj[5] = Node(BC2020);
-  adj[6] = Node(LA2019);
-  adj[7] = Node(LA2018);
-  adj[8] = Node(LA2017);
-  adj[9] = Node(MB2016);
-  adj[10] = Node(Mike);
-  adj[11] = Node(Tim);
-  adj[12] = Node(Katie);
-  adj[13] = Node(Callisto);
-  adj[14] = Node(Yu);
-  adj[15] = Node(Necati);
 
-  //connecting Teams to its Year
-  addTeamEdge(adj, 1, BC, 3, BC2022);
-  addTeamEdge(adj, 1, BC, 4, BC2021);
-  addTeamEdge(adj, 1, BC, 5, BC2020);
-  addTeamEdge(adj, 0, LA, 6, LA2019);
-  addTeamEdge(adj, 0, LA, 7, LA2018);
-  addTeamEdge(adj, 0, LA, 8, LA2017);
-  addTeamEdge(adj, 2, MB, 9, MB2016);
-  //connecting Years to its Team members
-  addTeamEdge(adj, 3, BC2022, 10, Mike);
-  addTeamEdge(adj, 4, BC2021, 10, Mike);
-  addTeamEdge(adj, 5, BC2020, 10, Mike);
-  addTeamEdge(adj, 6, LA2019, 11, Tim);
-  addTeamEdge(adj, 6, LA2019, 12, Katie);
-  addTeamEdge(adj, 7, LA2018, 12, Katie);
-  addTeamEdge(adj, 7, LA2018, 12, Katie);
-  addTeamEdge(adj, 8, LA2017, 12, Katie);
-  addTeamEdge(adj, 8, LA2017, 13, Callisto);
-  addTeamEdge(adj, 8, LA2017, 14, Yu);
-  addTeamEdge(adj, 9, MB2016, 14, Yu);
-  addTeamEdge(adj, 9, MB2016, 15, Necati);
-  
-  //connecting Teammates to Teammates
-  //condition: Connect if they are in the same team on the same year
-  addPlayerEdge(adj, 10, Mike, 11, Tim);
-  addPlayerEdge(adj, 11, Tim, 12, Katie);
-  addPlayerEdge(adj, 12, Katie, 13, Callisto);
-  addPlayerEdge(adj, 12, Katie, 14, Yu);
-  addPlayerEdge(adj, 14, Yu, 15, Necati);
-  printGraph(adj, V);
+  //Info holds name and index when its inside the vector
+  //-> Reason for index: Once you go thru adj node, U want to jump to where that adj node is in the adjacency list vector
+  //-> Visually, u want to go from the horizontal vector to the vertical vector. To jump to the right spot, U need to know its index
+  //--> Whats inside the adjacency list is NOT node obj, it is Info obj
+  Info LA("LA", 0);
+  Info BC("BC",1); 
+  Info MB("MB",2); 
+  Info BC2022("2022",3);
+  Info BC2021("2021",4);
+  Info BC2020("2020",5);
+  Info LA2019("2019",6);
+  Info LA2018("2018",7);
+  Info LA2017("2017",8);
+  Info MB2016("2016",9);
+  Info Mike("Mike",10); 
+  Info Tim("Tim",11); 
+  Info Katie("Katie",12);
+  Info Callisto("Callisto",13);
+  Info Yu("Yu",14);
+  Info Necati("Necati",15);
+
+  //Create Nodes from the Info and the type of info
+  //-> 0: Player Node, 1: Team Node, 2: Year Node
+  //--> Reason why we need "type" -> when traversing through the adj list, and lets say we are looping for all the players,
+  //--> we can simply identify which nodes are Player Node by the "Type"
+  //--> Or else, we will traverse thru ALL Team, Year, AND Player Nodes
+  adj.push_back(Node(LA,1));
+  adj.push_back(Node(BC,1));
+  adj.push_back(Node(MB,1));
+  adj.push_back(Node(BC2022,2));
+  adj.push_back(Node(BC2021,2));
+  adj.push_back(Node(BC2020,2));
+  adj.push_back(Node(LA2019,2));
+  adj.push_back(Node(LA2018,2));
+  adj.push_back(Node(LA2017,2));
+  adj.push_back(Node(MB2016,2));
+  adj.push_back(Node(Mike,0));
+  adj.push_back(Node(Tim,0));
+  adj.push_back(Node(Katie,0));
+  adj.push_back(Node(Callisto,0));
+  adj.push_back(Node(Yu,0));
+  adj.push_back(Node(Necati,0));
+
+  //Setting up Team adjacency with Year
+  addTeamEdge(adj, BC, BC2022);
+  addTeamEdge(adj, BC, BC2021);
+  addTeamEdge(adj, BC, BC2020);
+  addTeamEdge(adj, LA, LA2019);
+  addTeamEdge(adj, LA, LA2018);
+  addTeamEdge(adj, LA, LA2017);
+  addTeamEdge(adj, MB, MB2016);
+  //Setting up Year adjacency with Player
+  addTeamEdge(adj, BC2022, Mike);
+  addTeamEdge(adj, BC2021, Mike);
+  addTeamEdge(adj, BC2020, Mike);
+  addTeamEdge(adj, LA2019, Tim);
+  addTeamEdge(adj, LA2019, Katie);
+  addTeamEdge(adj, LA2018, Katie);
+  addTeamEdge(adj, LA2018, Katie);
+  addTeamEdge(adj, LA2017, Katie);
+  addTeamEdge(adj, LA2017, Callisto);
+  addTeamEdge(adj, LA2017, Yu);
+  addTeamEdge(adj, MB2016, Yu);
+  addTeamEdge(adj, MB2016, Necati);
+
+  //Once all "Black Edges" are connected properly, we now connect the "Red Edges"
+  //Set an algorithm that looks at ALL Year Nodes (type = 2), 
+  //And connect all adjacenty Players to each other
+  //--> Result of this: All players that played in the same team/same year will be "connected" by a red Edge.
+  //--> Distinguish "Red Edges" with addPlayerEdge. -> this appends adjacency into a different vector (**Look at Node class (private:))
+  addPlayerEdge(adj, Mike, Tim);
+  addPlayerEdge(adj, Tim, Katie);
+  addPlayerEdge(adj, Katie, Callisto);
+  addPlayerEdge(adj, Katie, Yu);
+  addPlayerEdge(adj, Yu, Necati);
+  printGraph(adj);
 
 }
 
 /*
+What we need to do::
+
 Case 1 (File) -> read inputfile
+  Given input fileName, make function called ReadFile(fileName, vector<Node> adj)
+  int index = 0; **for index to be stored inside Node (reason -> refer to line 104/105)
+  For each line of inputFile:
+    Parse into: Name, Team, Year **as strings
+    if(Name/Team/Year does not exist)
+      Make Info(string Name/Team/Year, index);
+      index++
+      adj.push_back(Info) add Nodes to adj list x3 for Name/Team/Year
+    if(Name/Team/Year does exist)
+      find it inside adj list to get its index (Reason: need to add edges so we need to find ALL THREE NODES)
+    addTeamEdge() -> add edges between these Name/Team/Year
+  After "Black edges" are fully made, we make the "Red edges"
+  For Every Year Node (Type = 2), connect all Players together inside its Player Vector (Refer to line 147)
+
+
 
 Case 2a (Player1, , ) -> print Player1's list of teams, sorted by year
   Get index of Player1 in array
